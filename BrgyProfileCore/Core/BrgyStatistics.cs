d46bbd2abc2fa7f76cfe86d81c9a91df34d9ccb3
jpteruel095 <jpteruel95@gmail.com>
2021-01-22 00:04:24 +0800

    Added few statistics

---
+++ BrgyProfileCore/Core/BrgyStatistics.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+namespace BrgyProfileCore.Core
+{
+    class BrgyStatistics
+    {
+        private static BrgyContext db = new BrgyContext();
+
+        public static int AverageResidentPerHousehold {
+            get {
+                var Households = db.Households.Include(h => h.Residents).ToList();
+                var totalResidents = Households.Select(h => h.Residents.Count).Sum();
+                var totalHouseholds = Households.Count;
+                return totalResidents/totalHouseholds;
+            }
+        }
+        public static int AveragePopulationperSitio
+        {
+            get
+            {
+                var Sitio = db.Sitio.Include(h => h.Residents).ToList();
+                var totalResidents = Sitio.Select(h => h.Residents.Count).Sum();
+                var totalSitio = Sitio.Count;
+                return totalResidents / totalSitio;
+            }
+        }
+        public static int AverageResident1_18perSitio
+        {
+            get
+            {
+                var Sitio = db.Sitio.Include(h => h.Residents).ToList();
+                var totalResidents = Sitio.Select<Sitio, int>(h =>
+                {
+                    var residents = h.Residents.Where(r => {
+                        return r.Age >= 0 && r.Age <= 18;
+                    }).ToList();
+                    return residents.Count;
+                }).Sum();
+                var totalSitio = Sitio.Count;
+                return totalResidents / totalSitio;
+            }
+        }
+        public static int AverageResident19_50perSitio
+        {
+            get
+            {
+                var Sitio = db.Sitio.Include(h => h.Residents).ToList();
+                var totalResidents = Sitio.Select<Sitio, int>(h =>
+                {
+                    var residents = h.Residents.Where(r => {
+                        return r.Age >= 19 && r.Age <= 50;
+                    }).ToList();
+                    return residents.Count;
+                }).Sum();
+                var totalSitio = Sitio.Count;
+                return totalResidents / totalSitio;
+            }
+        }
+        public static int AverageResident51_AboveperSitio
+        {
+            get
+            {
+                var Sitio = db.Sitio.Include(h => h.Residents).ToList();
+                var totalResidents = Sitio.Select<Sitio, int>(h =>
+                {
+                    var residents = h.Residents.Where(r => {
+                        return r.Age >= 51;
+                    }).ToList();
+                    return residents.Count;
+                }).Sum();
+                var totalSitio = Sitio.Count;
+                return totalResidents / totalSitio;
+            }
+        }
+    }
+
+    class Helper
+    {
+
+    }
+}
---
+++ BrgyProfileCore/CustomControls/StatisticDetailView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrgyProfileCore.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for ResidentDetailView.xaml
+    /// </summary>
+    public partial class StatisticDetailView : UserControl
+    {
+        /// <summary>
+        /// Field Header displayed on the left side
+        /// </summary>
+        public string FieldHeader
+        {
+            get
+            {
+                return (string)FieldHeaderLabel.Content;
+            }
+            set
+            {
+                FieldHeaderLabel.Content = value;
+            }
+        }
+
+        /// <summary>
+        /// Field Value displayed on the right side
+        /// </summary>
+        public string FieldValueText
+        {
+            get
+            {
+                return (string)FieldValueLabel.Content;
+            }
+            set
+            {
+                FieldValueLabel.Content = value;
+            }
+        }
+
+        public static readonly DependencyProperty FieldValueTextProperty = DependencyProperty.Register(
+            "FieldValueText",  // "ProductTitleText" ?
+            typeof(string),
+            typeof(StatisticDetailView), new UIPropertyMetadata(FieldValueTextChangeHandler));
+
+        public static void FieldValueTextChangeHandler(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get instance of current control from sender
+            // and property value from e.NewValue
+
+            // Set public property on TaregtCatalogControl, e.g.
+            ((StatisticDetailView)sender).FieldValueText = e.NewValue.ToString();
+        }
+        public StatisticDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
